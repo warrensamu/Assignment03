9b9b54f466d7329ab3076d635e7f3ec397750101
warrensamu <116026091+warrensamu@users.noreply.github.com>
2024-03-03 16:14:45 -0800

    Add files via upload

--- js/01-coinflip-redux.js
+++ js/01-coinflip-redux.js
@@ -1,5 +1,5 @@
 let coinFlip
-let numTimes = prompt('How many times do you want to flip the coin?')
+let numTimes = parseFloat(prompt('How many times do you want to flip the coin?'))
 
 for (let i = 1; i <= numTimes; i++) {
     let randomNum = Math.round(Math.random())
